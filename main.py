e0eddf5dfb25d5eb3503d7723130d93ec32044af
qr-oh <qiren14@gmail.com>
2023-06-03 22:19:31 +0800

    Added Basic GUI frame

---
+++ main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+# Declare main frame
+main_frame = Tk()
+main_frame.title("Merge PDFs")
+
+# Set parameters for GUI
+main_frame.configure(width=500, height=500)
+main_frame.configure(bg='lightgray')
+
+# Actual Program
+#---------------------------------------------
+def main():
+    main_frame.mainloop()
+
+if __name__ == "__main__":
+    main()
